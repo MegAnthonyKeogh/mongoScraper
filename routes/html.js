7009ecb82a354d3cea2acb609f86402b2e3e8de6
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-09-19 13:10:23 -0400

    getting closer

--- routes/html.js
+++ routes/html.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var mongoose = require("mongoose");
+var ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = function (app) {
 
@@ -22,15 +24,10 @@ module.exports = function (app) {
     app.get("/articles/:id", function (req, res) {
         // Create a new note and pass the req.body to the entry
         // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-        db.Article.findOne({
-                _id: req.params.id
-            })
-            // ..and populate all of the notes associated with it
+        db.Article.findOne({ _id: ObjectId(req.params.id)})
             .populate("note")
             .then(function (dbArticle) {
-                res.render('comment', {
-                    data: dbArticle
-                });
+                res.render('comment', { data: dbArticle });
             })
             .catch(function (err) {
                 // If an error occurred, send it to the client
