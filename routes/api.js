3f7cb9fcc47634bb1fb1310e93389e25058b2792
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-09-19 12:26:52 -0400

    new view

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ var mongoose = require("mongoose");
 var request =require("request");
 var cheerio = require("cheerio");
 var db = require("../models");
+var ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = function(app){
 
@@ -28,15 +29,6 @@ app.get("/scrape", function(req, res) {
           .attr("href")
           .trim();
           console.log(result.link);
-          // result.link = $(element)
-          // .children("a")
-          // .attr("href");
-          // result.p.teaser = $(element)
-          // .text();
-          // result.img = $(element)
-          // .children("a").find("img").attr("data-srcset").split(",")[0].split(" ")[0];
-          
-          
           
           db.Article.create(result)
           .then(function(dbArticle) {
@@ -57,10 +49,9 @@ app.get("/scrape", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
       .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, 
+          {set:{ note: dbNote._id }}, 
+          { new: true });
       })
       .then(function(dbArticle) {
         // If we were able to successfully update an Article, send it back to the client
@@ -71,5 +62,7 @@ app.get("/scrape", function(req, res) {
         res.json(err);
       });
   });
+
+
 
 }
--- routes/html.js
+++ routes/html.js
@@ -1,20 +1,41 @@
 var db = require("../models");
 
-module.exports = function(app){
+module.exports = function (app) {
 
 
 
-app.get('/', function (req, res) {
-    db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.render('home', {data:dbArticle});
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
+    app.get('/', function (req, res) {
+        db.Article.find({})
+            .then(function (dbArticle) {
+                // If we were able to successfully find Articles, send them back to the client
+                res.render('home', {
+                    data: dbArticle
+                });
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+
     });
-    
-});
+
+    app.get("/articles/:id", function (req, res) {
+        // Create a new note and pass the req.body to the entry
+        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+        db.Article.findOne({
+                _id: req.params.id
+            })
+            // ..and populate all of the notes associated with it
+            .populate("note")
+            .then(function (dbArticle) {
+                res.render('comment', {
+                    data: dbArticle
+                });
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
 
 }
